f233592940eb44f7fa4d2f14f10f8d7b30f2cc77
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2024-09-17 10:10:03 -0300

    Shared Context

--- src/main/java/br/com/alura/escola/MatricularAlunoPorLInhaDeComando.java
+++ src/main/java/br/com/alura/escola/MatricularAlunoPorLInhaDeComando.java
@@ -1,10 +1,17 @@
-package br.com.alura.escola.academico;
+package br.com.alura.escola;
+
+import java.util.List;
 
 import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAluno;
 import br.com.alura.escola.academico.aplicacao.aluno.matricular.MatricularAlunoDto;
-import br.com.alura.escola.academico.dominio.PublicadorDeEventos;
 import br.com.alura.escola.academico.dominio.aluno.LogDeAlunoMatriculado;
 import br.com.alura.escola.academico.infra.aluno.RepositorioDeAlunosEmMemoria;
+import br.com.alura.escola.gamificacao.aplicacao.GeraSeloAlunoNovato;
+import br.com.alura.escola.gamificacao.dominio.selo.RepositorioDeSelos;
+import br.com.alura.escola.gamificacao.dominio.selo.Selo;
+import br.com.alura.escola.gamificacao.infra.selo.RepositorioDeSelosEmMemoria;
+import br.com.alura.escola.shared.dominio.CPF;
+import br.com.alura.escola.shared.dominio.evento.PublicadorDeEventos;
 
 public class MatricularAlunoPorLInhaDeComando {
 	
@@ -17,6 +24,20 @@ public class MatricularAlunoPorLInhaDeComando {
 		
 		PublicadorDeEventos publicador = new PublicadorDeEventos();
 		publicador.adicionar(new LogDeAlunoMatriculado());
+		publicador.adicionar(new GeraSeloAlunoNovato(new RepositorioDeSelosEmMemoria() {
+			
+			@Override
+			public List<Selo> selosDoAlunoDeDPF(CPF cpf) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void adicionar(Selo selo) {
+				// TODO Auto-generated method stub
+				
+			}
+		}));
 		MatricularAluno matricular = new MatricularAluno(new RepositorioDeAlunosEmMemoria());
 		
 		matricular.executa(dto);
